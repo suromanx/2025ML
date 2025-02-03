805914de76805af92fc0215d333402a0d174c8e5
Roman Sukhoruchkin <societyorenb@mail.ru>
2025-02-03 16:53:16 +0300

    Добавил новый файл .idea/vcs.xml и изменил app.py

--- app.py
+++ app.py
@@ -6,4 +6,11 @@ app = FastAPI()
 @app.get("/")
 def say_hello():
     return 'hello'
+@app.get('/sum')
+def sum_two(a:int,b:int) -> int:
+    return a*b
+
+@app.get('/print/{number}')
+def print_num(number:int):
+    return number*2
 
